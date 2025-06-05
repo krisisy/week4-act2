884b59a6b6ffa5663739dedca6f89d19a94aaf39
CIITRFN001 <it@ciit.edu.ph>
2025-06-05 10:57:16 +0800

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +46,20 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class, 'user_role');
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->HasMany(Post::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->HasMany(Comment::class);
     }
 }
