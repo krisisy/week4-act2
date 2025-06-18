ad3cba188a15e85c935853077126151a7d8fdadf
Krisisy <khristine.isidro@ciit.edu.ph>
2025-06-19 02:38:27 +0800

    Made seeder template

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,16 +15,25 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
+        // Seeding Post then attach user id on it
         $users = User::all();
 
-        if ($users->count() == 0)
-        {
-            echo 'No users found, please run UserSeeder.';
+        if ($users->count() === 0) {
+            echo "No users found, please run UserSeeder first.\n";
             return;
         }
 
-        Post::factory(20)->create([
+        Post::factory(10)->create([
             'user_id' => $users->random()->id,
         ]);
+
+        // Attach categories of Post
+        $categories = Category::all();
+        $posts = Post::all();
+
+        foreach ($posts as $post) {
+            $randomCats = $categories->random(rand(1, 3));
+            $post->categories()->attach($randomCats);
+        }
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Seeding Users
+        User::factory(5)->create();
+
+        // Create user role entries
+        $adminRole = Role::where('role_name', 'Admin')->first();
+        $user = User::first(); // Fetch the first user from the database
+
+        if ($adminRole && $user) {
+            $user->roles()->attach($adminRole->id);
+        }
+
+        /////
+
+        $nonAdminRoles = Role::where('role_name', '!=', 'Admin')->get();
+        $nonAdminUsers = User::whereDoesntHave('roles', function ($query) {
+            $query->where('role_name', 'Admin');
+        })->get();
+
+        foreach ($nonAdminUsers as $nonAdminUser) {
+            $randomRoles = $nonAdminRoles->random(rand(1, 2));
+            $nonAdminUser->roles()->attach($randomRoles);
+        }
+    }
+}
