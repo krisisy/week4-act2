e1a1a63267e3fc2d9bdf69c737172081642107d9
Krisisy <khristine.isidro@ciit.edu.ph>
2025-06-18 22:02:52 +0800

    Finalized models

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Media extends Model
 {
+    use HasFactory;
+    
+    public $timestamps = false;
+
 
     protected $fillable = [
         'file_type',
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
+    use HasFactory;
+    
+    public $timestamps = false;
 
     protected $fillable = [
         'tag_name',
